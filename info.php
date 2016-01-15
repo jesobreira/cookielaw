845d57f284f4f9172c46871805072afd2727a094
Jefrey Sobreira Santos <jesobreira@yahoo.com.br>
2016-01-15 15:25:33 -0200

    Create info.php

---
+++ info.php
@@ -0,0 +1,16 @@
+<?php
+// Plugin Name
+$name = 'Cookie law FREE';
+
+// Plugin Author
+$author = 'phpDolphiners';
+
+// Plugin URL
+$url = 'http://phpdolphiners.cf';
+
+// Plugin Version
+$version = '1.0.0';
+
+// Plugin Type
+$type = '89';
+?>
